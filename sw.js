84dfc10bda5cd311cc90f73833ed232c5292ee72
juniorarlindo329-crypto <juniorarlindo329@gmail.com>
2026-09-15 17:49:40 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,4 @@
-const CACHE='podeconfiar-v19-1';
+const CACHE='podeconfiar-v20';
 self.addEventListener('install',()=>self.skipWaiting());
-self.addEventListener('activate',e=>e.waitUntil(
- caches.keys().then(keys=>Promise.all(keys.map(k=>k===CACHE?null:caches.delete(k))))
- .then(()=>self.clients.claim())
-));
-self.addEventListener('fetch',e=>{
- if(e.request.method==='GET') e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)));
-});
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k===CACHE?null:caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)));});
